182afb6d5b33bbd9baa3eb62c61b8e30ac3c8e90
hgeniee <hyun0325@sookmyung.ac.kr>
2025-05-13 01:03:21 +0900

    [AUTH] #5 스플래시 구현

--- app/src/main/java/com/moduro/barrier_free_app/presentation/auth/screen/SplashScreen.kt
+++ app/src/main/java/com/moduro/barrier_free_app/presentation/auth/screen/SplashScreen.kt
@@ -1,13 +1,22 @@
 package com.moduro.barrier_free_app.presentation.auth.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.moduro.barrier_free_app.R
+import com.moduro.barrier_free_app.core_ui.theme.MainYellow
 import kotlinx.coroutines.delay
 
 @Composable
@@ -23,7 +32,23 @@ fun SplashScreen(navController: NavController, modifier: Modifier = Modifier){
         }
     }
 
-    Text("스플래시 화면입니다.")
+    
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MainYellow)
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_start_logo),
+                contentDescription = "moduro logo"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
