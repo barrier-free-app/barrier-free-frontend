4366670a53bf36ea8eed18b654491dbe437833f1
Kim Nahyeon <nhyeonii@naver.com>
2025-06-13 14:57:59 +0900

    [FEAT] #8 공통 Chip 생성

--- app/src/main/java/com/moduro/barrier_free_app/core_ui/component/Chip.kt
+++ app/src/main/java/com/moduro/barrier_free_app/core_ui/component/Chip.kt
@@ -1,4 +1,49 @@
 package com.moduro.barrier_free_app.core_ui.component
 
-class Chip {
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.moduro.barrier_free_app.core_ui.theme.Button1
+import com.moduro.barrier_free_app.core_ui.theme.Text5
+
+@Composable
+fun FacilityChip(
+    label: String
+) {
+    Surface(
+        shape = RoundedCornerShape(10.dp),
+        color = Button1,
+        modifier = Modifier
+            .height(30.dp)
+            .padding(end = 8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(horizontal = 12.dp)
+        ) {
+            Text(
+                text = label,
+                fontSize = 14.sp,
+                color = Text5
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun FacilityChipPreview() {
+    FacilityChip(
+        label = "🛗 엘리베이터"
+    )
 }
