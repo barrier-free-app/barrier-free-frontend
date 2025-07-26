c4c6170fb97348e14c9b5e0c83f9eba48539e5ee
seoyeonsw <c2c4@naver.com>
2025-07-27 01:52:17 +0900

    [FEAT] #28 장소 검색 관련 ui 구현

---
+++ app/src/main/java/com/moduro/barrier_free_app/presentation/search/screen/SearchScreenTop.kt
@@ -0,0 +1,118 @@
+package com.moduro.barrier_free_app.presentation.search.screen
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.moduro.barrier_free_app.R
+import com.moduro.barrier_free_app.core_ui.theme.Background2
+import com.moduro.barrier_free_app.core_ui.theme.LocalbarrierFreeTypographyProvider
+import com.moduro.barrier_free_app.core_ui.theme.Text2
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreenTop(
+    search: String,
+    onBackClick : () -> Unit,
+    onSearchChange: (String) -> Unit,
+    onSearchClick : () -> Unit,
+    //onFilterClick : () -> Unit
+) {
+    val typography = LocalbarrierFreeTypographyProvider.current
+
+    Column (
+        modifier = Modifier.background(Background2).padding(horizontal = 8.dp).padding(bottom = 15.dp)
+    ){
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            Image(
+                modifier = Modifier.width(11.dp).height(22.dp).clickable { onBackClick() },
+                painter = painterResource(R.drawable.ic_back), contentDescription = "뒤로가기")
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                modifier = Modifier.height(22.dp).width(22.dp),
+                //clickable { onFilterClick() }
+                painter = painterResource(R.drawable.map_filter), contentDescription = "필터")
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        TextField(
+            modifier =  Modifier.fillMaxWidth(),
+            value = search ,
+            onValueChange = onSearchChange,
+
+            shape = RoundedCornerShape(10.dp),
+            placeholder = {
+                Text(
+                    text = "원하는 장소를 검색하세요.",
+                    color = Text2,
+                    style = typography.H5_SB_5
+                )
+            },
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Text2,
+                unfocusedTextColor = Text2,
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            trailingIcon = {
+                IconButton(onClick = onSearchClick) {
+                    Icon(
+                        painter = painterResource(R.drawable.map_search), // ← 돋보기 아이콘
+                        contentDescription = "검색",
+                        tint = Color.Gray
+                    )
+                }
+            },
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun SearchScreenTopPreview() {
+
+    SearchScreenTop(
+        "원하는 장소를 검색하세요.",
+        onBackClick = {},
+        {},
+        {},
+    )
+
+}
